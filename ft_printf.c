849a4be2aa83c6a6123b4f37e7432dcabb863ae6
Kugumi <pavialavia@gmail.com>
2019-08-08 23:57:36 +0300

    almost deal with stdarg

--- ft_printf.c
+++ ft_printf.c
@@ -1,24 +1,28 @@
-#include <printf.h>
+#include "ft_printf.h"
 
-int ft_ spec(const char * restrict format)
+int ft_spec(const char * restrict format)
 {
 	int f;
 
-	f = 0;
+	f = 1;
 	if (format[f] == '%')
+		return (0);
+	while (format[f] != 'c' || format[f] != 'd' || format[f] != 'p' || format[f] != 'i' || format[f] != 'o' || format[f] != 'u' || format[f] != 'x' || format[f] !=
+				'X' || format[f] != 'f' || format[f] != 'l' || format[f] != 'h' || format[f] != 'L')
 	{
-		f++;
-		if (format[f] == '%')
-			return (0);
-		while(format[f] != '%')
+		if (format[f] == '-')
 		{
-
+			write(1, "bya", 3);
 		}
 	}
+	return (1);
 }
 
-int printf(const char * restrict format, ...)
+int ft_printf(const char * restrict format, ...)
 {
+	va_list args;
+	va_start(args, format);
+
 	int i;
 	int f;
 
@@ -26,7 +30,23 @@ int printf(const char * restrict format, ...)
 	f = 0;
 	while (format[f])
 	{
-
+		while (format[f] == '%')
+		{
+			if ((ft_spec(format + f)) == 0)
+			{
+				write(1, "%", 1);
+				i++;
+				f += 2;
+			}
+		}
+		write(1, &format[f], 1);
+		i++;
+		f++;
 	}
+	va_end(args);
 	return (i);
+}
+int main()
+{
+	ft_printf("%%");
 }
