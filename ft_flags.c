07e10a1505f01150ff17a7bd46a1543f852fe3d3
Kugumi <pavialavia@gmail.com>
2019-09-06 21:17:00 +0300

    ft_flags.c

--- ft_flags.c
+++ ft_flags.c
@@ -1,12 +1,12 @@
 #include "ft_printf.h"
 
-int ft_flagsdi(const char *restrict fo, t_flags *fl)
+int ft_flagsdi(const char *restrict fo, t_flags *fl, t_args *ag)
 {
 	int f;
 
 	f = 0;
 	while (fo[f] != 'd' && fo[f] != 'i' && fo[f] != 'l' && fo[f] != 'h'
-			&& fo[f] != '.' && !(fo[f] >= 49 && fo[f] <= 57))
+			&& fo[f] != '.' && !(fo[f] >= 49 && fo[f] <= 57) && fo[f] != '*')
 	{
 		if (fo[f] == '-')
 			fl->mi = !(fl->ze = 0) ? 1 : 1;
@@ -20,18 +20,18 @@ int ft_flagsdi(const char *restrict fo, t_flags *fl)
 			return (-1);
 		f++;
 	}
-	if (fo[f] >= 48 && fo[f] <= 57)
+	if ((fo[f] >= 48 && fo[f] <= 57) || fo[f] == '*')
 	{
-		ft_wdh(fo + f, fl);
-		while (fo[f] >= 48 && fo[f] <= 57)
+		ft_wdh(fo + f, fl, ag);
+		while ((fo[f] >= 48 && fo[f] <= 57) || fo[f] == '*')
 			f++;
 	}
 	if (fo[f] == '.')
 	{
 		fl->ze = 0;
-		ft_psn(fo + f, fl);
+		ft_psn(fo + f, fl, ag);
 		f++;
-		while (fo[f] >= 48 && fo[f] <= 57)
+		while ((fo[f] >= 48 && fo[f] <= 57) || fo[f] == '*')
 			f++;
 	}
 	if (fo[f] != 'd' && fo[f] != 'i' && fo[f] != 'l' && fo[f] != 'h')
@@ -39,13 +39,13 @@ int ft_flagsdi(const char *restrict fo, t_flags *fl)
 	return (1);
 }
 
-int ft_flagscsp(const char *restrict fo, t_flags *fl, unsigned long long l)
+int ft_flagscsp(const char *restrict fo, t_flags *fl, unsigned long long l, t_args *ag)
 {
 	int f;
 
 	f = 0;
 	while (fo[f] != 'c' && fo[f] != 's' && fo[f] != 'p' && fo[f] != '.'
-			&& !(fo[f] >= 49 && fo[f] <= 57))
+			&& !(fo[f] >= 49 && fo[f] <= 57) && fo[f] != '*')
 	{
 		if (fo[f] == '-')
 			fl->mi = 1;
@@ -55,15 +55,15 @@ int ft_flagscsp(const char *restrict fo, t_flags *fl, unsigned long long l)
 	}
 	if (fo[f] >= 48 && fo[f] <= 57)
 	{
-		ft_wdh(fo + f, fl);
-		while (fo[f] >= 48 && fo[f] <= 57)
+		ft_wdh(fo + f, fl, ag);
+		while ((fo[f] >= 48 && fo[f] <= 57) || fo[f] == '*')
 			f++;
 	}
 	if (fo[f] == '.' && fo[l - 2] == 's')
 	{
-		ft_psn(fo + f, fl);
+		ft_psn(fo + f, fl, ag);
 		f++;
-		while (fo[f] >= 48 && fo[f] <= 57)
+		while ((fo[f] >= 48 && fo[f] <= 57) || fo[f] == '*')
 			f++;
 	}
 	if (fo[f] != 'c' && fo[f] != 's' && fo[f] != 'p')
@@ -71,13 +71,13 @@ int ft_flagscsp(const char *restrict fo, t_flags *fl, unsigned long long l)
 	return (1);
 }
 
-int ft_flagsf(const char *restrict fo, t_flags *fl)
+int ft_flagsf(const char *restrict fo, t_flags *fl, t_args *ag)
 {
 	int f;
 
 	f = 0;
 	while (fo[f] != 'f' && fo[f] != '.' && fo[f] != 'l' && fo[f] != 'L' &&
-			!(fo[f] >= 49 && fo[f] <= 57))
+			!(fo[f] >= 49 && fo[f] <= 57) && fo[f] != '*')
 	{
 		if (fo[f] == '-')
 			fl->mi = !(fl->ze = 0) ? 1 : 1;
@@ -95,15 +95,15 @@ int ft_flagsf(const char *restrict fo, t_flags *fl)
 	}
 	if (fo[f] >= 48 && fo[f] <= 57)
 	{
-		ft_wdh(fo + f, fl);
-		while (fo[f] >= 48 && fo[f] <= 57)
+		ft_wdh(fo + f, fl, ag);
+		while ((fo[f] >= 48 && fo[f] <= 57) || fo[f] == '*')
 			f++;
 	}
 	if (fo[f] == '.')
 	{
-		ft_psn(fo + f, fl);
+		ft_psn(fo + f, fl, ag);
 		f++;
-		while (fo[f] >= 48 && fo[f] <= 57)
+		while ((fo[f] >= 48 && fo[f] <= 57) || fo[f] == '*')
 			f++;
 	}
 	if (fo[f] != 'f' && fo[f] != 'l' && fo[f] != 'L')
@@ -111,13 +111,13 @@ int ft_flagsf(const char *restrict fo, t_flags *fl)
 	return (1);
 }
 
-int ft_flagsu(const char *restrict fo, t_flags *fl)
+int ft_flagsu(const char *restrict fo, t_flags *fl, t_args *ag)
 {
 	int f;
 
 	f = 0;
 	while (fo[f] != 'u' && fo[f] != '.' && fo[f] != 'l' && fo[f] != 'h'
-			&& !(fo[f] >= 49 && fo[f] <= 57))
+			&& !(fo[f] >= 49 && fo[f] <= 57) && fo[f] != '*')
 	{
 		if (fo[f] == '-')
 		{
@@ -135,16 +135,16 @@ int ft_flagsu(const char *restrict fo, t_flags *fl)
 	}
 	if (fo[f] >= 48 && fo[f] <= 57)
 	{
-		ft_wdh(fo + f, fl);
-		while (fo[f] >= 48 && fo[f] <= 57)
+		ft_wdh(fo + f, fl, ag);
+		while ((fo[f] >= 48 && fo[f] <= 57) || fo[f] == '*')
 			f++;
 	}
 	if (fo[f] == '.')
 	{
 		fl->ze = 0;
-		ft_psn(fo + f, fl);
+		ft_psn(fo + f, fl, ag);
 		f++;
-		while (fo[f] >= 48 && fo[f] <= 57)
+		while ((fo[f] >= 48 && fo[f] <= 57) || fo[f] == '*')
 			f++;
 	}
 	if (fo[f] != 'u' && fo[f] != 'l' && fo[f] != 'h')
@@ -152,13 +152,13 @@ int ft_flagsu(const char *restrict fo, t_flags *fl)
 	return (1);
 }
 
-int ft_flagsoxX(const char *restrict fo, t_flags *fl)
+int ft_flagsoxX(const char *restrict fo, t_flags *fl, t_args *ag)
 {
 	int f;
 
 	f = 0;
 	while (fo[f] != 'o' && fo[f] != 'x' && fo[f] != 'X' && fo[f] != '.'
-			&& fo[f] != 'l' && fo[f] != 'h' && !(fo[f] >= 49 && fo[f] <= 57))
+			&& fo[f] != 'l' && fo[f] != 'h' && !(fo[f] >= 49 && fo[f] <= 57) && fo[f] != '*')
 	{
 		if (fo[f] == '-')
 		{
@@ -175,16 +175,16 @@ int ft_flagsoxX(const char *restrict fo, t_flags *fl)
 	}
 	if (fo[f] >= 48 && fo[f] <= 57)
 	{
-		ft_wdh(fo + f, fl);
-		while (fo[f] >= 48 && fo[f] <= 57)
+		ft_wdh(fo + f, fl, ag);
+		while ((fo[f] >= 48 && fo[f] <= 57) || fo[f] == '*')
 			f++;
 	}
 	if (fo[f] == '.')
 	{
 		fl->ze = 0;
-		ft_psn(fo + f, fl);
+		ft_psn(fo + f, fl, ag);
 		f++;
-		while (fo[f] >= 48 && fo[f] <= 57)
+		while ((fo[f] >= 48 && fo[f] <= 57) || fo[f] == '*')
 			f++;
 	}
 	if (fo[f] != 'o' && fo[f] != 'x' && fo[f] != 'X' && fo[f] != 'h' &&
